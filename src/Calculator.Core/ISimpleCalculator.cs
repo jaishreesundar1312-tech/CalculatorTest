0b7d7edc89395ba09372a966a020ab70e4e7d6d7
jaishreesundar1312-tech <jaishreesundar1312@gmail.com>
2026-06-27 01:25:08 +0100

    Update ISimpleCalculator.cs

--- src/Calculator.Core/ISimpleCalculator.cs
+++ src/Calculator.Core/ISimpleCalculator.cs
@@ -2,7 +2,6 @@ namespace Calculator.Core;
 
 /// <summary>
 /// Contract for a simple integer calculator, exactly as supplied
-/// in the Renaissance Global programming challenge.
 /// </summary>
 public interface ISimpleCalculator
 {
